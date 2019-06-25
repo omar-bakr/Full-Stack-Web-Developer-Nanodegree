3e55fc135488f67570db08eeb0c530cec11b9030
omar-bakr <omar.bakr@student.guc.edu.eg>
2019-06-25 20:47:18 +0200

    format the the print statements

--- Project 1/logs_analysis.py
+++ Project 1/logs_analysis.py
@@ -8,29 +8,40 @@ DBNAME = "news"
 def get_best3_articles():
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
-  c.execute("select replace(path,'/article/',''),views from best_articles ;")
+  c.execute("select replace(path,'/article/',''),views from best3_articles ;")
   result= c.fetchall()
-  print(result)
+  print "The most popular three articles are :- "
+  for title, views in result:
+        print '{} with {} views '.format(title, views)
   db.close()
+
+
 
 """problem 2 :-Who are the most popular article authors of all time ? """
 def get_best_authtor():
+  print("---------------------------------------------")
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
   c.execute("select name,sum_views from best_author,authors where author=authors.id ;")
   result= c.fetchall()
-  print(result)
+  print("The most popular article author is :-")
+  for author, views in result:
+        print 'The author "{}" has {} views'.format(author, views)
   db.close()
 
 """problem 3 :- On which days did more than 1% of requests lead to errors? ? """
 def get_err_day():
+  print("---------------------------------------------")
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
   c.execute('''select all_requests.mydate, round((err.count*1.0/all_requests.count)*100,2) 
   as percentage from all_requests,err where all_requests.mydate=err.mydate and 
   ((err.count*1.0/all_requests.count)*100) > 1 ;''')
   result= c.fetchall()
-  print(result)
+  (date,err)=result[0]
+  print( "The days were more than 1% of requests lead to error :-")
+  print 'On day {} the error was {}'.format(date,err)
+  
   db.close()
 
 if __name__ == '__main__':
