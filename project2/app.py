3bc6120a0b4416b76453adfc8ae11ebe3774c0a2
omar-bakr <omar.bakr@student.guc.edu.eg>
2019-08-28 14:51:54 +0200

    pep8

--- project2/app.py
+++ project2/app.py
@@ -1,23 +1,20 @@
-from flask import Flask, render_template, request, redirect,jsonify, url_for, flash
+import requests
+import json
+import string
+import random
+import httplib2
+from flask import session as login_session
+from flask import flash, make_response
+from oauth2client.client import FlowExchangeError
+from oauth2client.client import flow_from_clientsecrets
+from database_setup import Category, Item, Base, User
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, asc
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 app = Flask(__name__)
 
-from sqlalchemy import create_engine, asc
-from sqlalchemy.orm import sessionmaker
-from database_setup import Category,Item,Base,User
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
 
-
-from flask import flash, make_response
-from flask import session as login_session
-
-import httplib2
-import random
-import string
-import json
-import requests
-
-#Connect to Database and create database session
+# Connect to Database and create database session
 engine = create_engine('sqlite:///item-catalog.db')
 Base.metadata.bind = engine
 
@@ -33,6 +30,7 @@ def showLogin():
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
+
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
     if request.args.get('state') != login_session['state']:
@@ -42,7 +40,6 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
@@ -51,7 +48,6 @@ def fbconnect():
         app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-
 
     # Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.8/me"
@@ -104,6 +100,7 @@ def fbconnect():
     flash("Now logged in as %s" % login_session['username'])
     return output
 
+
 @app.route('/fbdisconnect')
 def fbdisconnect():
     access_token = login_session.get('access_token')
@@ -115,7 +112,8 @@ def fbdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     login_session.clear()
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (
+        facebook_id, access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[0]
     if result['status'] != '200':
@@ -124,14 +122,13 @@ def fbdisconnect():
     return render_template('logged_out.html')
 
 
-
-#Routes
+# Routes
 @app.route('/')
 @app.route('/catalog')
 def home():
     categories = session.query(Category).all()
-    items=session.query(Item).all()
-    logged_in=is_logged_in()
+    items = session.query(Item).all()
+    logged_in = is_logged_in()
     return render_template('index.html',
                            categories=categories,
                            items=items,
@@ -139,11 +136,12 @@ def home():
                            section_title="Latest Items",
                            )
 
+
 @app.route('/catalog/<string:category_name>')
 def categoryItems(category_name):
     categories = session.query(Category).all()
     items = session.query(Item).filter_by(category_name=category_name).all()
-    logged_in=is_logged_in()
+    logged_in = is_logged_in()
     return render_template('index.html',
                            categories=categories,
                            current_category=category_name,
@@ -152,6 +150,7 @@ def categoryItems(category_name):
                            section_title="%s Items (%d items)" % (
                                category_name, len(items)),
                            )
+
 
 @app.route('/catalog/<string:category_name>/<string:item_name>')
 def OneItem(category_name, item_name):
@@ -163,6 +162,7 @@ def OneItem(category_name, item_name):
                            user_id=user_id,
                            logged_in=logged_in,
                            )
+
 
 @app.route('/catalog/new-item', methods=['GET', 'POST'])
 def addNewItem():
@@ -289,25 +289,34 @@ def createUser(login_session):
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
-#JSON end-points
+# JSON end-points
 @app.route('/catalog.json')
 def catalogjson():
     categories = session.query(Category).all()
-    items=session.query(Item).all()
-    return jsonify(categories=[c.serialize for c in categories],Items=[i.serialize for i in items])
+    items = session.query(Item).all()
+    return jsonify(
+        categories=[
+            c.serialize for c in categories], Items=[
+            i.serialize for i in items])
+
 
 @app.route('/catalog/<string:category_name>.json')
 def categoryItemsjson(category_name):
     categories = session.query(Category).all()
     items = session.query(Item).filter_by(category_name=category_name).all()
-    return jsonify(categories=[c.serialize for c in categories],Items=[i.serialize for i in items])
+    return jsonify(
+        categories=[
+            c.serialize for c in categories], Items=[
+            i.serialize for i in items])
+
 
 @app.route('/catalog/<string:category_name>/<string:item_name>.json')
 def OneItemjson(category_name, item_name):
     item = session.query(Item).filter_by(name=item_name).one()
     return jsonify(Items=[i.serialize for i in items])
 
-#user helper functions
+# user helper functions
+
 
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
@@ -315,24 +324,26 @@ def createUser(login_session):
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
-    return user.id   
+    return user.id
 
 
 def getUserInfo(user_id):
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
+
 def getUserID(email):
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
-    except:
+    except BaseException:
         return None
+
 
 def is_logged_in():
     return login_session.get('access_token') is not None
 
-    
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
--- project2/database_setup.py
+++ project2/database_setup.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
 
@@ -13,17 +13,15 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    email= Column(String(250), unique=True ,nullable=False)
-    picture= Column(String(250))
-
+    email = Column(String(250), unique=True, nullable=False)
+    picture = Column(String(250))
 
 
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), unique=True,nullable=False)
-
+    name = Column(String(250), unique=True, nullable=False)
 
     @property
     def serialize(self):
@@ -32,19 +30,18 @@ class Category(Base):
             'name': self.name,
             'id': self.id,
         }
+
 
 class Item(Base):
     __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), unique=True,nullable=False)
+    name = Column(String(250), unique=True, nullable=False)
     description = Column(String(250))
-    user_id = Column(Integer,ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
-    category_name = Column(Integer,ForeignKey('category.name'))
+    category_name = Column(Integer, ForeignKey('category.name'))
     category = relationship(Category)
-
-
 
     @property
     def serialize(self):
@@ -52,13 +49,9 @@ class Item(Base):
         return {
             'name': self.name,
             'id': self.id,
-            'description':self.description,
-            'category_name':self.category_name,
+            'description': self.description,
+            'category_name': self.category_name,
         }
-
-
-
-
 
 
 engine = create_engine('sqlite:///item-catalog.db')
